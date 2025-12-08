3fafc1326afd1949db0a5180c873515f24fde267
Roman Yarosh <yarosh0106@gmail.com>
2025-12-09 00:53:54 +0300

    commit

--- src/test/java/PracticeFormDemoqa.java
+++ src/test/java/PracticeFormDemoqa.java
@@ -15,7 +15,6 @@ public class PracticeFormDemoqa {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 5000;
     }
     
     @Test
