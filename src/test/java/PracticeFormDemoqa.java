64021d4e65db6518fd32c7d0d71e03aa6635284b
Roman Yarosh <yarosh0106@gmail.com>
2025-12-13 18:11:52 +0300

    added Attach.java

--- src/test/java/PracticeFormDemoqa.java
+++ src/test/java/PracticeFormDemoqa.java
@@ -1,4 +1,8 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -26,6 +30,16 @@ public class PracticeFormDemoqa {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
     
     @Test
